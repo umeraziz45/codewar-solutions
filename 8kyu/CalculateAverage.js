64fda523b35f99276a46a259b42e6786a395bc7c
umeraziz <umeraziz4545@gmail.com>
2022-10-30 12:34:11 -0600

    calculate average of arrays

---
+++ 8kyu/CalculateAverage.js
@@ -0,0 +1,19 @@
+function findAverage(array) {
+  // your code here
+  let sum = 0;
+  
+  if(array === false){
+    return 0
+  }else{
+    array.forEach((element)=>{
+      sum += element;
+    })
+    let ave = sum / array.length;
+      return ave 
+  }
+
+}
+
+let arr = [1, 2, 3, 4]
+
+findAverage(arr);
